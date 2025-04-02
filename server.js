ab65d653b235d0646ca1b276a7ac620592b791cf
uditmaltare <uditmaltare@gmail.com>
2025-04-02 14:37:16 +0530

    run

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use("/todo", routes)
 connectDB().then(() => {
     try {
 
-        app.listen(3000, () => console.log('Server is running on port 3000')
+        app.listen(3000, () => console.log('Server is running on port 3000 run')
 
         )
     }
